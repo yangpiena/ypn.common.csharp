51be8a4c806ad6f2c84b98a94d5879a1ae5b6186
yangpiena <yangpiena@163.com>
2018-10-10 01:16:52 +0800

    add class

---
+++ ypn.common.csharp/SecretHelper.cs
@@ -0,0 +1,22 @@
+﻿/**
+* 命名空间： ypn.common.csharp
+*
+* 功    能： 加密解密的工具类
+* 类    名： SecretHelper
+*
+* 版本  变更日期            负责人   变更内容
+* ───────────────────────────────────
+* V0.01 2018/10/10 01:15:53 YPN      初版
+*
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ypn.common.csharp
+{
+    class SecretHelper
+    {
+    }
+}
